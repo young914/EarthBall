fab593e896b623521417cd256ca5a87a8b858dce
Slubp <dgpp12@naver.com>
2023-05-29 13:18:46 +0900

    멤버폴더 기본 틀
    멤버 폴더에
    
    Contoller / vo / dao / service, serviceImpl 를 추가하여 작업하기 전 기초적인 폴더 구조를 잡아뒀습니다!

---
+++ src/main/java/com/earthball/member/model/service/MemberService.java
@@ -0,0 +1,7 @@
+package com.earthball.member.model.service;
+
+public interface MemberService {
+  
+           //   서비스 인터페이스 입니다.
+  
+}
---
+++ src/main/java/com/earthball/member/model/service/MemberServiceImpl.java
@@ -0,0 +1,7 @@
+package com.earthball.member.model.service;
+
+public class MemberServiceImpl  {
+  
+  // 서비스입니다
+
+}
