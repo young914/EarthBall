9d807c50c7426b602a106f97a5cca77e55350b13
Slubp <dgpp12@naver.com>
2023-05-29 14:40:44 +0900

    Update MemberController.java

--- src/main/java/com/earthball/member/controller/MemberController.java
+++ src/main/java/com/earthball/member/controller/MemberController.java
@@ -4,10 +4,7 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
-<<<<<<< Updated upstream
-=======
-  
-    
->>>>>>> Stashed changes
+
+  // 주석1 멤버 컨트롤
 
 }
