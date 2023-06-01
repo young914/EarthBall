5b80615469bbfbfc2df2f52c85280471f730f266
LIKEJU <sunjuuuu@naver.com>
2023-06-01 17:08:28 +0900

    보드vo

---
+++ src/main/java/com/earthball/member/board/model/vo/Board.java
@@ -0,0 +1,27 @@
+package com.earthball.member.board.model.vo;
+
+import java.sql.Date;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+
+public class Board {
+
+  private int boardNo;
+  private String boardTitle;
+  private String boardWriter;
+  private String boardContent;
+  private String originName;
+  private String changeName;
+  private int count;
+  private String createDate; // 날짜 타입을 문자열로 표기 가능, TO_CHAR 를 사용
+  private String status;
+
+
+}
