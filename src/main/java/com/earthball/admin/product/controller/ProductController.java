e38ff1502e31eb8ba16da092c3382a71d7f2f9f9
insu choi <cth7097@gmail.com>
2023-06-01 18:10:36 +0900

    관리자페이지 상품추가 구현중-
    인수

--- src/main/java/com/earthball/admin/product/controller/ProductController.java
+++ src/main/java/com/earthball/admin/product/controller/ProductController.java
@@ -1,7 +1,11 @@
 package com.earthball.admin.product.controller;
 
+import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import com.earthball.admin.product.model.vo.Product;
+import com.earthball.common.template.ChangeFileName;
 
 @Controller("admin.Controller")
 public class ProductController {
@@ -14,6 +18,29 @@ public class ProductController {
   @RequestMapping("enrollForm.pro")
   public String enrollForm() {
     return "admin/product/productEnrollForm";
+  }
+
+  @RequestMapping("insert.pro")
+  public void insertProduct(Product p,
+                            MultipartFile upfile1, MultipartFile upfile2, MultipartFile upfile3, MultipartFile upfile4,
+                            HttpSession session) {
+
+    System.out.println(upfile1);
+
+    String changeName1 = null;
+    String changeName2 = null;
+    String changeName3 = null;
+    String changeName4 = null;
+
+//    if(!upfile1.getOriginalFilename().equals("")) {
+//      changeName1 = ChangeFileName.saveFile(upfile1, session);
+//      changeName2 = ChangeFileName.saveFile(upfile2, session);
+//      changeName3 = ChangeFileName.saveFile(upfile2, session);
+//      changeName4 = ChangeFileName.saveFile(upfile2, session);
+    }
+
+
+
   }
 
 }
