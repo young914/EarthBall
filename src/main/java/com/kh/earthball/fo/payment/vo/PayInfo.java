889d37ab12868fdee7120f1820f814674777025a
jgt0814 <jgt0814@naver.com>
2023-06-02 18:21:09 +0900

    결제 시 DB에 값 추가 기능 추가
    sql문 오류 또는 넘어가는 값에 오류있음 (수정 필요)
    DB에 PAYMENT 테이블에 ADDRESS 컬럼과 DETAIL_ADDRESS 컬럼명 변경 필요

---
+++ src/main/java/com/kh/earthball/fo/payment/vo/PayInfo.java
@@ -0,0 +1,28 @@
+package com.kh.earthball.fo.payment.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class PayInfo {
+
+  private String paymentNo;
+  private String memberId;
+  private String paymentType;
+  private int paymentTotal;
+  private String receiveName;
+  private int receivePhone;
+  private int postCode;
+  private String receiveAddress1;
+  private String receiveAddress2;
+  private String paymentToken;
+}
--- src/main/webapp/resources/fo/js/payment/paymentPageForm.js
+++ src/main/webapp/resources/fo/js/payment/paymentPageForm.js
@@ -24,70 +24,84 @@ function orderPay() {
 
     IMP.init("imp43570318"); // 가맹점 식별코드
 
-    console.log($(".product_name").html());
-    console.log($(".totalAmount").html());
-    console.log($("#email").html());
-    console.log($("#name").html());
-    console.log($("#phone").html());
-    console.log($("#address1").val() + $("#address2").val());
-    console.log($("#postCode").val());
-
     IMP.request_pay({
       pg: "kakaopay.TC0ONETIME",	// kcp, kakaopay, +
       pay_method: "kakaopay",
-      merchant_uid: "2023053100002",   // 주문번호
+      merchant_uid: 'merchant_' + new Date().getTime(),   // 주문번호
       name: $(".product_name").html(),
       amount: $(".totalAmount").html(),                         // 숫자 타입
       buyer_email: $("#email").html(),
       buyer_name: $("#name").html(),
-      buyer_tel: $(".receivePhone").html(),
+      buyer_tel: $("#receivePhone").val(),
       buyer_addr: $("#address1").val() + $("#address2").val(),
       buyer_postcode: $("#postCode").val()
     }, function (rsp) { // callback
       //rsp.imp_uid 값으로 결제 단건조회 API를 호출하여 결제결과를 판단합니다.
 		console.log(rsp);
 
+		// console.log("rep.success : " + rsp.success);
+
 		if(rsp.success) {
 
-			var msg = "결제 완료";
-            msg += '고유ID : ' + rsp.imp_uid;                //아임포트 uid는 실제 결제 시 결제 고유번호를 서버와 비교해서 결제처리하는데 필요없긴함.
-            msg += '// 상점 거래ID : ' + rsp.merchant_uid;
-            msg += '// 결제 금액 : ' + rsp.paid_amount;
-            msg += '// 카드 승인번호 : ' + rsp.apply_num;
+			// var msg = "결제 완료";
+      //       msg += '고유ID : ' + rsp.imp_uid;                //아임포트 uid는 실제 결제 시 결제 고유번호를 서버와 비교해서 결제처리하는데 필요없긴함.
+      //       msg += '// 상점 거래ID : ' + rsp.merchant_uid;
+      //       msg += '// 결제 금액 : ' + rsp.paid_amount;
+      //       msg += '// 카드 승인번호 : ' + rsp.apply_num;
 
-            // $.ajax({
-            // 	url : "/paySuccess",
-            // 	type : "post",
-            // 	data : {
-			// 		paymentNo : rsp.merchant_uid,
-			// 		memberId : ${ loginUser.memberId },
-			// 		paymentType : rsp.pg_provider,
-			// 		paymentTotal : rsp.paid_amount,
-			// 		receiveName : rsp.buyer_name,
-			// 		receivePhone : rsp.buyer_tel,
-			// 		postCode : rsp.buyer_postcode,
-			// 		receiveAddress1 : $("#address1").val(),
-			// 		receiveAddress2 : $("#address2").val(),
-			// 		paymentToken : rsp.pg_tid
-			// 		},
-			// 		success : function() {
+			console.log(rsp.imp_uid);
 
-			// 			var msg = "결제가 완료되었습니다.";
-			// 		},
-			// 		error : function() {
+			console.log("----------");
+			console.log(rsp.merchant_uid);
+			console.log($('.sessionMemberId').val());
+			console.log(rsp.pg_provider);
+			console.log(rsp.paid_amount);
+			console.log(rsp.buyer_name);
+			console.log(rsp.buyer_tel);
+			console.log(rsp.buyer_postcode);
+			console.log(rsp.pg_tid);
+			console.log("----------");
 
-			// 		}
-            // });
+            $.ajax({
+            	url : "/paySuccess",
+            	type : "post",
+            	// async : true,
+            	data : {
+					paymentNo : rsp.merchant_uid,
+					memberId : $('.sessionMemberId').val(),
+					paymentType : rsp.pg_provider,
+					paymentTotal : rsp.paid_amount,
+					receiveName : rsp.buyer_name,
+					receivePhone : rsp.buyer_tel,
+					postCode : rsp.buyer_postcode,
+					receiveAddress1 : $("#address1").val(),
+					receiveAddress2 : $("#address2").val(),
+					paymentToken : rsp.pg_tid
+					},
+					success : function(data) {
+
+						console.log(data);
+						var msg = "결제가 완료되었습니다.";
+						alert(msg);
+					},
+					error : function() {
+
+						console.log("ajax 실패");
+						var msg = "결제에 실패하였습니다." + rsp.error_msg;
+						alert(msg);
+
+					}
+            });
 
 		} else {
-			console.log($(".name").html());
 
-			var msg = "결제 실패"
+			var msg = "결제 실패";
         	msg += "에러 내용" + rsp.error_msg;
+        	alert(msg);
 		}
 
-		alert(msg);
-		document.location.href="/payment.pa";
+		console.log("뭐지?");
+		// document.location.href="/payment.pa";
 	});
 
 }
