1bb2874647efd6115d0fe5d6eed8b1411f3cac25
TaeSick <sun0571027@gmail.com>
2023-06-07 18:14:32 +0900

    필터링1 완성

--- src/main/java/com/kh/earthball/fo/store/controller/StoreController.java
+++ src/main/java/com/kh/earthball/fo/store/controller/StoreController.java
@@ -4,14 +4,12 @@ import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.google.gson.Gson;
-import com.kh.earthball.fo.common.template.Pagination;
-import com.kh.earthball.fo.common.vo.PageInfo;
 import com.kh.earthball.fo.store.service.StoreService;
 import com.kh.earthball.fo.store.template.GeocodingApi;
+import com.kh.earthball.fo.store.vo.Region;
 import com.kh.earthball.fo.store.vo.Store;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -55,11 +53,62 @@ public class StoreController {
   */
   
   @GetMapping("storeListView.st")
-  public String selectList() {
+  public String selectList(Model model) {
     
-   
+ // 전체 다 가져오기 
+    ArrayList<Region> regionList = storeService.selectRegion();
+
+    // 시/도 만 가져오기
+    ArrayList<Region> cityList = storeService.selectCityList();
     
+    
+    model.addAttribute("regionList", regionList);
+    model.addAttribute("cityList", cityList);
     return "fo/store/storeListView";
+  }
+  
+  @ResponseBody
+  @GetMapping(value = "getCities.st", produces = "application/json; charset=UTF-8")
+  public String getCityFilter(String city, Model model) {
+    
+    ArrayList<Region> provincesList = storeService.selectProvincesList(city); 
+    
+    return new Gson().toJson(provincesList);
+ 
+  }
+  
+  @ResponseBody
+  @GetMapping(value = "getFilter.st", produces = "application/json; charset=UTF-8")
+  public String getFilterList(String city, String provinces, Model model) {
+    
+    System.out.println(city);
+    System.out.println(provinces);
+    
+    int regionNo = storeService.selectRegionNo(city, provinces);
+    System.out.println(regionNo);
+    
+    ArrayList<Store> selectFilterList = storeService.selectFilterList(regionNo);
+    
+    
+
+    for (int i = 0; i < selectFilterList.size(); i++) {
+
+      GeocodingApi geocodingApi = new GeocodingApi();
+      double[] coordinates = geocodingApi.getGeocode(selectFilterList.get(i).getStoreAddress());
+      double latitude = coordinates[0];
+      double longitude = coordinates[1];
+      String jibunAddress = geocodingApi.getJibunAddress(latitude, longitude);
+
+      selectFilterList.get(i).setStoreLat(latitude); // Store 객체에 위도 값 설정
+      selectFilterList.get(i).setStoreLon(longitude); // Store 객체에 경도 값 설정
+      selectFilterList.get(i).setJibunAddress(jibunAddress); // Store 객체에 지번 주소 값 설정
+    }
+
+    
+    
+    System.out.println(selectFilterList);
+    return new Gson().toJson(selectFilterList);
+ 
   }
   
   @ResponseBody
@@ -84,6 +133,5 @@ public class StoreController {
     return new Gson().toJson(list);
   }
   
-  
-
+ 
 }
